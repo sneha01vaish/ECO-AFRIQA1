7c00980513cd192fcdbeaa4a994b712f859ece37
unknown <allanmathenge2.com>
2024-07-18 03:21:09 +0300

    footer section

--- freshly_set/frontend/src/components/Home.js
+++ freshly_set/frontend/src/components/Home.js
@@ -21,7 +21,7 @@ function Home() {
      <Faq />
 
      <Contact />  
-    {/* <FreshlyFooter /> */}
+    <FreshlyFooter />
      </div>
   )
 }
--- freshly_set/frontend/src/components/footer/FreshlyFooter.js
+++ freshly_set/frontend/src/components/footer/FreshlyFooter.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { FaPhoneAlt, FaFacebookF,FaArrowUp, FaInstagramSquare } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-import { BsTwitterX } from "react-icons/bs";
+import { FaArrowUp } from "react-icons/fa";
 
 const FreshlyFooter = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   const toggleVisibility = () => {
-    if (window.pageYOffset > 300) {
+    if (window.pageYOffset > 800) {
       setIsVisible(true);
       // console.log("window scroll to height > 300", isVisible)
     } else {
@@ -30,64 +28,70 @@ const FreshlyFooter = () => {
   }, []);
 
   return (
-    <div className="bg-green-300 text-[10px] md:text-lg text-gray-600 p-4 sm:text-sm mt-[160px]">
-      <div className="text-xs md:text-lg flex flex-col justify-center items-start text-gray-600 lg:flex-row border-b border-green-400 pb-8">
+    <div className="p-4">
+      <div className="w-[90%] bg-green-700 mx-auto pb-8 rounded-xl">
+        <div className="flex justify-around items-center py-6 text-gray-600 gap-8">
 
-        <section className="">
-          <h1 className="text-center font-bold pb-2">LOGO</h1>
-          <div className="hidden lg:flex">
-            <div className="gap-4">
+          <section className="hidden sm:flex">
 
-              <p className="">We are revolutionizing urban farming by transforming urban spaces into lush, green sanctuaries, bringing farm-fresh goodness closer to your table. Join us</p>
-              <div className="pt-2 flex gap-2">
-                <a href="+254 712345678"><FaPhoneAlt /></a>
-                <a href= "mailto: info@freshlyfarms.com"><MdEmail /></a>
-              </div>
+            <div className="bg-white rounded-full p-2">
+              <img src="/static/media/logo2.png" alt="Freshly farms logo" />
             </div>
+
+          </section> 
+
+          <div className="flex flex-wrap justify-center items-start gap-6">
+
+            <div className="text-nowrap flex flex-col">
+              <h4 className="font-bold text-gray-100 underline">Pages</h4>
+              <a href="/">Home</a>
+              <a href="tutor">Services</a>
+              <a href="students">Resources</a>
+              <a href="about">Our History</a>
+              <a href="contact">Contact Us</a>
+            </div>
+
+            <div className="text-nowrap flex flex-col">
+              <h4 className="font-bold text-gray-100 underline">Explore</h4>
+              <a href="blog">Blog</a>
+              <a href="faq">FAQs</a>
+              <a href="terms">Terms of Service</a>
+              <a href="policy">Privacy Policy</a>
+            </div>
+
+            <div className="text-nowrap flex flex-col">
+              <h4 className="font-bold text-gray-100 underline">Services</h4>
+              <a href="about">Personal Info</a>
+              <a href="tutor">Orders</a>
+              <a href="students">Adresses</a>
+              <a href="parents">Parents</a>
+              <a href="contact">New Password</a>
+            </div>
+
+            <div className="text-nowrap flex flex-col">
+              <h4 className="font-text-gray-100 text-gray-100 underline">Pages</h4>
+              <a href="/">Home</a>
+              <a href="tutor">Services</a>
+              <a href="students">Resources</a>
+              <a href="about">Our History</a>
+              <a href="contact">Contact Us</a>
+            </div>
+
           </div>
-        </section>
 
-        <div className="flex justify-center items-start gap-6 sm:gap-4 md:gap-8 mx-auto">
-
-          <div className="text-nowrap flex flex-col">
-            <h4 className="font-bold pb-2 underline">Pages</h4>
-            <a href="/">Home</a>
-            <a href="tutor">Services</a>
-            <a href="students">Resources</a>
-            <a href="about">Our History</a>
-            <a href="contact">Contact Us</a>
+          <div className="p-1 fixed bottom-4 right-4">
+          {isVisible && (
+            <button
+            onClick={scrollToTop}
+            className="bg-green-600 text-xs p-1 px-1.5 animate-ping cursor-pointer rounded-full text-white border-none transition">
+            <FaArrowUp />
+          </button>
+          )}
           </div>
 
-          <div className="text-nowrap flex flex-col">
-            <h4 className="font-bold pb-2 underline">Explore</h4>
-            <a href="blog">Blog</a>
-            <a href="faq">FAQs</a>
-            <a href="terms">Terms of Service</a>
-            <a href="policy">Privacy Policy</a>
-          </div>
-
-          <div className="text-nowrap flex flex-col">
-            <h4 className="font-bold pb-2 underline">Services</h4>
-            <a href="about">Personal Info</a>
-            <a href="tutor">Orders</a>
-            <a href="students">Adresses</a>
-            <a href="parents">Parents</a>
-            <a href="contact">New Password</a>
-          </div>
-        </div>
-
-        <div className="animate-ping fixed bottom-4 right-4 ">
-        {isVisible && (
-          <button
-          onClick={scrollToTop}
-          className="bg-green-600 p-2 text-white rounded-full shadow-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition">
-          <FaArrowUp />
-        </button>
-        )}
-        </div>
-
-      </div> 
-        {/* <hr className="w-full h-[2px] bg-green-500" /> */}
+        </div> 
+        <hr className="w-full bg-gray-200" />
+      </div>
     </div>
   )
 }
