31ba0391df3d8a3627161d9040170084a609af38
unknown <allanmathenge2.com>
2024-07-18 04:32:50 +0300

    metrics section

--- freshly_set/frontend/src/components/Home.js
+++ freshly_set/frontend/src/components/Home.js
@@ -3,11 +3,12 @@ import Hero from './Hero'
 import Mission from './Mission'
 import Reviews2 from './Reviews2'
 import Nav from './Nav'
-import FreshlyFooter from "../components/footer/FreshlyFooter";
 import Contact from "./Contact";
 import Faq from './Faq'
+import Metrics from '../components/metrics/Metrics';
 import Services from './Services'
 import ProductSection from './ProductSection'
+import FreshlyFooter from '../components/footer/FreshlyFooter'
 function Home() {
   return (
     <div className="">
@@ -15,6 +16,7 @@ function Home() {
 
      <Hero />
      <Mission />
+     <Metrics />
      <Services />
      <ProductSection />
      <Reviews2 />
