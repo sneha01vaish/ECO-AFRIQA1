b5350100879b307a8df2333e9e9f2beda1bd17b6
Simanga <simacoder@hotmail>
2024-07-18 19:20:27 +0200

    'secured'

--- freshly_set/freshly_set/settings.py
+++ freshly_set/freshly_set/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from decouple import config, Csv
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,12 +117,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 #SMTP Configuration
 
+# Load environment variables from .env file
+load_dotenv()
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = 'True'
-EMAIL_HOST_USER = 'info.freshlyfarms5@gmail.com'
-EMAIL_HOST_PASSWPRD = ''
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
