9221745ce14767ee9b063fc9239d5c6251341266
CoderLawe <sosahlawe@gmail.com>
2024-07-19 14:23:56 +0200

    updated metrics sizing

--- freshly_set/frontend/src/components/footer/FreshlyFooter.js
+++ freshly_set/frontend/src/components/footer/FreshlyFooter.js
@@ -35,55 +35,67 @@ const FreshlyFooter = () => {
   return (
     <div className="p-4 py-8">
       <div className="w-full bg-[#008000] mx-auto pb-8 rounded-xl">
-        <div className="flex justify-around items-center py-8 text-gray-600 gap-8">
-
-          <section className="hidden sm:flex">
-
-            <div className="bg-white rounded-full p-2">
-              <img className="h-[113px] w-[120px]" src="/static/media/logo2.png" alt="Freshly farms logo" /> 
+        <div className=" lg:flex justify-around items-center py-8 text-gray-600 gap-8">
+ 
+ 
+          <section className="flex">
+ 
+ 
+            <div className="bg-white rounded-full p-2 mx-auto">
+              <img className="h-[113px] w-[120px]" src="/static/media/logo2.png" alt="Freshly farms logo" />
             </div>
-
-          </section> 
-
-          <div className="flex flex-wrap justify-center items-start gap-8">
-
-            <div className="text-nowrap flex flex-col">
-              <h4 className="font-bold text-gray-100 underline">Pages</h4>
-              <Link className="" to="/">Home</Link>
-              <Link to="tutor">Services</Link>
-              <Link to="students">Resources</Link>
-              <Link to="about">Our History</Link>
-              <Link to="/contact">Contact Us</Link>
+ 
+ 
+          </section>
+ 
+ 
+          <div className="flex lg:flex-wrap justify-center items-start gap-x-[40px]">
+ 
+ 
+            <div className="text-nowrap flex flex-col space-y-[8px]">
+              <h4 className="font-bold text-gray-100 ">Quick Link</h4>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="/">Home</Link>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="tutor">Services</Link>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="students">Resources</Link>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="about">Our History</Link>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="contact">Contact Us</Link>
+             
             </div>
-
-            <div className="text-nowrap flex flex-col">
-              <h4 className="font-bold text-gray-100 underline">Explore</h4>
-              <Link to="blog">Blog</Link>
-              <Link to="faq">FAQs</Link>
-              <Link to="terms">Terms of Service</Link>
-              <Link to="policy">Privacy Policy</Link>
+ 
+ 
+            <div className="text-nowrap flex flex-col space-y-[8px]">
+              <h4 className="font-bold text-gray-100 ">Explore</h4>
+              <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="blog">Blog</Link>
+              <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="faq">FAQs</Link>
+              <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="terms">Terms of Service</Link>
+              <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="policy">Privacy Policy</Link>
             </div>
-
-            <div className="text-nowrap flex flex-col">
-              <h4 className="font-bold text-gray-100 underline">Services</h4>
-              <Link to="about">Personal Info</Link>
-              <Link to="tutor">Orders</Link>
-              <Link to="students">Adresses</Link>
-              <Link to="parents">Parents</Link>
-              <Link to="contact">New Password</Link>
+ 
+ 
+            <div className="text-nowrap flex flex-col space-y-[8px]">
+              <h4 className="font-bold text-gray-100 ">Services</h4>
+              <Link  className="text-gray-200 font-inter  text-[15px] font-[500] underline" to="about">Careers</Link>
+              <Link  className="text-gray-200 font-inter text-[15px] font-[500] underline" to="tutor">Blog</Link>
+              <Link  className="text-gray-200 font-inter text-[15px] font-[500] underline" to="students">Farming Techniques</Link>
+              <Link  className="text-gray-200 font-inter text-[15px] font-[500] underline" to="parents">Become a Member</Link>
+              <Link  className="text-gray-200 font-inter text-[15px]  font-[500] underline" to="contact">Officer</Link>
             </div>
-
-            <div className="text-nowrap flex flex-col">
-              <h4 className="font-text-gray-100 text-gray-100 underline">Pages</h4>
-              <Link to="/">Home</Link>
-              <Link to="tutor">Services</Link>
-              <Link to="students">Resources</Link>
-              <Link to="about">Our History</Link>
-              <Link to="/contact">Contact Us</Link>
+ 
+ 
+            <div className="text-nowrap flex flex-col space-y-[8px]">
+              <h4 className="font-bold text-gray-100 ">Quick Link</h4>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="/">Home</Link>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="tutor">Services</Link>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="students">Resources</Link>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="about">Our History</Link>
+                <Link className="text-gray-200 font-inter text-[15px] font-[500]" to="contact">Contact Us</Link>
+             
             </div>
-
+ 
+ 
           </div>
-
+ 
+ 
           <div className="p-1 fixed bottom-4 right-4">
           {isVisible && (
             <button
@@ -93,32 +105,15 @@ const FreshlyFooter = () => {
           </button>
           )}
           </div>
-
-        </div> 
+ 
+ 
+        </div>
         <hr className="w-full bg-gray-200" />
-
-        <section className="flex flex-wrap gap-2 justify-around items-center text-xs">
-          <div>
-            <span className="text-gray-200">&copy;{year}-</span>
-            <Link to="/">Freshly Farms.</Link>
-            <span className="text-gray-200">All Rights Reserved</span>
-          </div>
-          <div>
-            <Link to="/"><FaSquareInstagram className="mx-2" /></Link>
-            <Link to="https://www.tiktok.com/@infofkexnoo?_t=8o7P91f39aI&_r=1" target="_blank"><FaTiktok className="mx-2" /></Link>
-            <Link to="https://www.facebook.com/profile.php?id=61561757183894" target="_blank"><FaFacebook className="mx-2" /></Link>
-            <Link to="https://x.com/FreshlyFarms5" target="_blank"><FaSquareXTwitter className="mx-2" /></Link>
-            <Link to="/"><FaLinkedin className="mx-2" /></Link>
-          </div>
-          <div className="flex gap-2">
-            <Link to="/">Terms and conditions</Link>
-            <Link to="/">Privacy Policy</Link>
-            <Link to="/">Support</Link>
-          </div>
-        </section>
       </div>
     </div>
   )
-}
+ }
+     
+
 
 export default FreshlyFooter
