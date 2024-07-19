44bc1d2285a435c0ad5cebaf0587b6664c4745fb
Sneha Vaish <snehavaish44@gmail.com>
2024-07-20 01:13:43 +0530

    added login, logout , signup pages and updated url.py, views.py file

--- freshly_set/freshlyapp/forms.py
+++ freshly_set/freshlyapp/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Product, Service, Blog
 from django.utils.text import slugify
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
