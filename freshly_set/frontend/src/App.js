dba6885e49a7bbd1ab581a0559f0fb4f709a510e
Photolo <photolo.gift@gmail.com>
2024-07-19 05:15:36 +0200

    Sign-Up

--- freshly_set/frontend/src/App.js
+++ freshly_set/frontend/src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import About from './components/pages/about-us/About';
 import Home from "./components/pages/homepage/Home"
-import LoginSignup from './components/pages/login/LoginSignup';
+import SignUp from './components/pages/login/LoginSignup';
+
 function App() {
   return (
     <Router>
     <Routes>
-        <Route path="/login-signup" element={<LoginSignup />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<About />} />
     </Routes>
