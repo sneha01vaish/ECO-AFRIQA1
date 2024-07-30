8b3a3b08d4220c4eb82372802bd66ffc7569c4c4
unknown <allanmathenge2.com>
2024-07-30 19:04:06 +0300

    ui complete

--- freshly_set/frontend/src/components/pages/farm-produce/ProduceCenter.js
+++ freshly_set/frontend/src/components/pages/farm-produce/ProduceCenter.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Deals from './Deals'
+import Popular from './Popular'
 
 const ProduceCenter = () => {
   return (
-    <main className=' bg-white/80'>
+    <main className='bg-white/80 sm:max-w-[70%] w-full mx-auto py-16'>
         <Deals />
+        <Popular />
     </main>
   )
 }
