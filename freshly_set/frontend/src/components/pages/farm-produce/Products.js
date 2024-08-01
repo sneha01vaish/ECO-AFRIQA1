05296fe88ea446a9602f96894675fb12bb0be876
CoderLawe <sosahlawe@gmail.com>
2024-08-01 05:09:29 +0200

    hero section first commit

---
+++ freshly_set/frontend/src/components/pages/farm-produce/Products.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from '../../Nav/Navbar'
+import ProduceCenter from './ProduceCenter'
+import ProductsHero from './ProductsHero'
+
+function Products() {
+  return (
+    <div>
+        <Nav />
+        <ProductsHero />
+        <ProduceCenter />
+
+    </div>
+  )
+}
+
+export default Products
