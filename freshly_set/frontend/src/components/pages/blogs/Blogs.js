81475d2d4cd42c28ee5fb91822d777811cb7271a
CoderLawe <sosahlawe@gmail.com>
2024-08-02 05:35:49 +0200

    fixed detail page issue

--- freshly_set/frontend/src/components/pages/blogs/Blogs.js
+++ freshly_set/frontend/src/components/pages/blogs/Blogs.js
@@ -23,28 +23,29 @@ const Blogs = () => {
     setVisible((prevCount) => Math.max(prevCount - 3, 3));
   };
 
- useEffect(() => {
+//  useEffect(() => {
 
-  const fetchBlogs = async () => {
-    try {
-      const response = await api.get('freshlyapp/blogs');
-      setBlogs(response.data);
+//   const fetchBlogs = async () => {
+//     try {
+//       const response = await api.get('freshlyapp/blogs');
+//       setBlogs(response.data);
 
-    } catch (error) {
-      if (error) {
-        // Catch errors out of 200 range
-        console.log(error.response.data)
-        console.log(error.response.headers)
-        console.log(error.response.status)
-      } else {
-        // any other errors within 200
-        console.log(error.response.message)
-      }
-    };
-    fetchBlogs();
-    console.log("Blogs fetched", blogs)
-  }, []);
+//     } catch (error) {
+//       if (error) {
+//         // Catch errors out of 200 range
+//         console.log(error.response.data)
+//         console.log(error.response.headers)
+//         console.log(error.response.status)
+//       } else {
+//         // any other errors within 200
+//         console.log(error.response.message)
+//       }
+//     };
+//     fetchBlogs();
+//     console.log("Blogs fetched", blogs)
+//   }, [] };
 
+ 
   return (
     <div>
       <BlogSearch/>
