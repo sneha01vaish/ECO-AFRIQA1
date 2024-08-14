739456417b37eb462fa77f61b58ca23e35062c98
CoderLawe <sosahlawe@gmail.com>
2024-08-14 10:01:34 +0200

    added detail pages

--- freshly_set/frontend/src/App.js
+++ freshly_set/frontend/src/App.js
@@ -1,29 +1,28 @@
 import './App.css';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import About from './components/pages/about-us/About';
-
 import Home from "./components/pages/homepage/Home"
 import LoginSignUp from "./components/pages/SignUp/LoginSignUp";
 import Blogs from "./components/pages/blogs/Blogs";
-import Categories from "./components/pages/farm-produce/Categories"
-import Details from './components/pages/about-us/Details';
 import Products from './components/pages/farm-produce/Products';
-import Detailed from './components/pages/homepage/Detailed';
-
+import FarmingSystemsDetail from './components/pages/farm-produce/FarmingSystemsDetail';
+import GardenSetupsDetail from './components/pages/farm-produce/GardenSetupsDetail';
+import Detail from "./components/pages/homepage/Detailed"
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/SignUp" element={<LoginSignUp />} />
+        <Route path="/" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
-      
-        <Route path="/products/categories" element={<Categories />} />
         <Route path="/products" element={<Products />} />
         <Route path="/about-us" element={<About />} />
-        <Route path="/team-detail" element={<Details />} />
-        <Route path="/reviews2-detailed" element={<Detailed />} />
+        <Route path="/team-detail" element={<Detail />} />
+        <Route path="/farmingSystems" element={<FarmingSystemsDetail />}/>
+        <Route path="/gardenSetups" element={<GardenSetupsDetail />}/>
+        {/* <Route path="/api/blogs" element={<Blogs />} /> */}
       </Routes>
     </Router>
   );
