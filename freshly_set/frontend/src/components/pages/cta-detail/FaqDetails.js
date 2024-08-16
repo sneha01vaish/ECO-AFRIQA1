01b5cd3fcaed0f7fdb5c10d203a86e3026182720
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-08-17 01:15:16 +0300

    The Desktop Version of the cta for faq section

--- freshly_set/frontend/src/components/pages/cta-detail/FaqDetails.js
+++ freshly_set/frontend/src/components/pages/cta-detail/FaqDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { FaCaretDown } from "react-icons/fa6";
+import { FaCaretUp } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 import Nav from '../../Nav/Navbar'
@@ -32,6 +32,7 @@ function FaqDetails() {
     <div>
         <Nav />  {/*The navbar */}
         
+        {/*Detailed FAQ Section Starts Here */}
         <div className='FaqDetails relative top-[100px] mx-[40px] '>
           <div className='TopClass md:flex justify-between '>
             {/*Left */}
@@ -61,73 +62,173 @@ function FaqDetails() {
             </div>
             
           </div>   {/*Top Class Ends Here */}
-          
-        <div className='SetOfQuestions'>
-        <div className='flex justify-center w-[100%]'>
-            {/* Card 1*/}
-        
-            <div className=" block mx-1 lg:mx-auto w-[50%] rounded-[21px] border-solid border-[1px] border-black/[0.25] mb-4 shadow-lg">
-                <p className="text-black font-inter  lg:ml-0 text-[12px] lg:text-[22px] font-[700] ">What Is Vertical Farming?</p>
-                <div className="flex justify-between items-center align-top">
-                    <p className={clicked[1] ? "text-black/[0.62] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[9px] lg:text-[16px]" : "hidden"}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la
-                    </p>
-                    <div className="flex justify-end w-[100%]">
-                        <FaCaretDown onClick={() => toggleFAQ(1)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[40px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[1] ? "rotate-180" : ""}`} />
+
+        {/*Sample Call-To_Action FAQs */}      
+        <div className='SetOfQuestions '>
+            {/*Questions 1 and 2 */}
+            <div className='Qn1AndQn2 lg:flex justify-between '>
+                {/* Card 1*/}
+                <div className='Card1 block lg:mx-0 lg:w-[47%] h-fit'>
+                    {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                    
+                    <div className="CardContent  rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-[50px] shadow-lg">
+                        <p className="text-black font-inter -ml-[10px] lg:ml-0 text-justify text-[12px] lg:text-[22px] font-[700]"> What Is Urban Farming?</p>
+                        <div className="flex justify-between ">
+                            <p className={clicked[1] ? "text-black/[0.62] w-[500%] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[10px] lg:text-[20px] text-justify" : "hidden"}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la </p>
+                            <div className="flex justify-end w-[100%]">
+                                <FaCaretUp  onClick={() => toggleFAQ(1)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[59px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[1] ? "rotate-180" : ""}`} />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </div> {/*Card1 ends here */}
 
-
-        {/* Card 2*/}
-        <div className=" block mx-1 lg:mx-auto w-[40%] rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-4 shadow-lg">
-                <p className="text-black font-inter -ml-[10px] lg:ml-0 text-[12px] lg:text-[22px] font-[700]">What Is Urban Farming?</p>
-                <div className="flex justify-between items-center align-top">
-                    <p className={clicked[2] ? "text-black/[0.62] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[9px] lg:text-[16px]" : "hidden"}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la
-                    </p>
-                    <div className="flex justify-end w-[100%]">
-                    <FaCaretDown 
-                        onClick={() => toggleFAQ(2)} 
-                        className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[40px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[2] ? "rotate-180" : ""}`}
-                    />
+                {/* Card 2*/}
+                <div className='Card2 block lg:mx-0 lg:w-[47%] h-fit'>
+                    {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                    
+                    <div className="CardContent  rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-[50px] shadow-lg">
+                        <p className="text-black font-inter -ml-[10px] lg:ml-0 text-justify text-[12px] lg:text-[22px] font-[700]"> What Is Urban Farming?</p>
+                        <div className="flex justify-between ">
+                            <p className={clicked[2] ? "text-black/[0.62] w-[500%] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[10px] lg:text-[20px] text-justify" : "hidden"}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la </p>
+                            <div className="flex justify-end w-[100%]">
+                                <FaCaretUp  onClick={() => toggleFAQ(2)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[59px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[2] ? "rotate-180" : ""}`} />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-       
-              
-
-        
+                </div> {/*Card2 ends here */} 
     
-        </div>
+            </div> {/*Cards 1 and 2 Ends Here */}
         
-        
-                  
+            {/*Questions 3 and 4 */}
+            <div className='Qn3AndQn4 lg:flex justify-between '>
+                {/* Card 3*/}
+                <div className='Card3 block lg:mx-0 lg:w-[47%] h-fit'>
+                    {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                    
+                    <div className="CardContent  rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-[50px] shadow-lg">
+                        <p className="text-black font-inter -ml-[10px] lg:ml-0 text-justify text-[12px] lg:text-[22px] font-[700]"> What Is Urban Farming?</p>
+                        <div className="flex justify-between ">
+                            <p className={clicked[3] ? "text-black/[0.62] w-[500%] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[10px] lg:text-[20px] text-justify" : "hidden"}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la </p>
+                            <div className="flex justify-end w-[100%]">
+                                <FaCaretUp  onClick={() => toggleFAQ(3)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[59px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[3] ? "rotate-180" : ""}`} />
+                            </div>
+                        </div>
+                    </div>
+                </div> {/*Card3 ends here */}
 
+                {/* Card 4*/}
+                <div className='Card4 block lg:mx-0 lg:w-[47%] h-fit'>
+                    {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                    
+                    <div className="CardContent  rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-[50px] shadow-lg">
+                        <p className="text-black font-inter -ml-[10px] lg:ml-0 text-justify text-[12px] lg:text-[22px] font-[700]"> What Is Urban Farming?</p>
+                        <div className="flex justify-between ">
+                            <p className={clicked[4] ? "text-black/[0.62] w-[500%] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[10px] lg:text-[20px] text-justify" : "hidden"}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la </p>
+                            <div className="flex justify-end w-[100%]">
+                                <FaCaretUp  onClick={() => toggleFAQ(4)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[59px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[4] ? "rotate-180" : ""}`} />
+                            </div>
+                        </div>
+                    </div>
+                </div> {/*Card4 ends here */} 
+    
+            </div> {/*Cards 3 and 4 Ends Here */}
 
+             {/*Questions 5 and 6 */}     
+            <div className='Qn5AndQn6 lg:flex justify-between '>
+                {/* Card 5*/}
+                <div className='Card5 block lg:mx-0 lg:w-[47%] h-fit'>
+                    {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                    
+                    <div className="CardContent  rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-[50px] shadow-lg">
+                        <p className="text-black font-inter -ml-[10px] lg:ml-0 text-justify text-[12px] lg:text-[22px] font-[700]"> What Is Urban Farming?</p>
+                        <div className="flex justify-between ">
+                            <p className={clicked[5] ? "text-black/[0.62] w-[500%] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[10px] lg:text-[20px] text-justify" : "hidden"}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la </p>
+                            <div className="flex justify-end w-[100%]">
+                                <FaCaretUp  onClick={() => toggleFAQ(5)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[59px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[5] ? "rotate-180" : ""}`} />
+                            </div>
+                        </div>
+                    </div>
+                </div> {/*Card5 ends here */}
 
+                {/* Card 6*/}
+                <div className='Card6 block lg:mx-0 lg:w-[47%] h-fit'>
+                    {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                    
+                    <div className="CardContent  rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-[50px] shadow-lg">
+                        <p className="text-black font-inter -ml-[10px] lg:ml-0 text-justify text-[12px] lg:text-[22px] font-[700]"> What Is Urban Farming?</p>
+                        <div className="flex justify-between ">
+                            <p className={clicked[6] ? "text-black/[0.62] w-[500%] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[10px] lg:text-[20px] text-justify" : "hidden"}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la </p>
+                            <div className="flex justify-end w-[100%]">
+                                <FaCaretUp  onClick={() => toggleFAQ(6)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[59px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[6] ? "rotate-180" : ""}`} />
+                            </div>
+                        </div>
+                    </div>
+                </div> {/*Card6 ends here */} 
+    
+            </div> {/*Cards 5 and 6 Ends Here */}
 
+             {/*Questions 7 and 8 */}
+            <div className='Qn7AndQn8 lg:flex justify-between '>
+                {/* Card 7*/}
+                <div className='Card7 block lg:mx-0 lg:w-[47%] h-fit'>
+                    {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                    
+                    <div className="CardContent  rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-[50px] shadow-lg">
+                        <p className="text-black font-inter -ml-[10px] lg:ml-0 text-justify text-[12px] lg:text-[22px] font-[700]"> What Is Urban Farming?</p>
+                        <div className="flex justify-between ">
+                            <p className={clicked[7] ? "text-black/[0.62] w-[500%] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[10px] lg:text-[20px] text-justify" : "hidden"}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la </p>
+                            <div className="flex justify-end w-[100%]">
+                                <FaCaretUp  onClick={() => toggleFAQ(7)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[59px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[7] ? "rotate-180" : ""}`} />
+                            </div>
+                        </div>
+                    </div>
+                    {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                </div> {/*Card7 ends here */}
+
+                {/* Card 8*/}
+                <div className='Card8 block lg:mx-0 lg:w-[47%] h-fit'>
+                    {/*Line */}
+                    <div className=' lg:border-solid lg:border-[5px] lg:mb-[50px] lg:border-[#008000]' />
+                    
+                    <div className="CardContent  rounded-[21px] border-solid border-[1px] border-black/[0.25] pl-[22px] mb-[50px] shadow-lg">
+                        <p className="text-black font-inter -ml-[10px] lg:ml-0 text-justify text-[12px] lg:text-[22px] font-[700]"> What Is Urban Farming?</p>
+                        <div className="flex justify-between ">
+                            <p className={clicked[8] ? "text-black/[0.62] w-[500%] -ml-[8px] lg:ml-0 -mt-[10px] lg:-mt-[16px] font-josefin flex text-[10px] lg:text-[20px] text-justify" : "hidden"}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la </p>
+                            <div className="flex justify-end w-[100%]">
+                                <FaCaretUp  onClick={() => toggleFAQ(8)} className={`text-black h-[30px] lg:h-[52px] w-[20.8px] lg:w-[55px] -mt-[59px] mr-[14px] cursor-pointer transition-all duration-300 ease-in-out ${clicked[8] ? "rotate-180" : ""}`} />
+                            </div>
+                        </div>
+                    </div>
+                     {/*Line */}
+                    <div className=' border-solid border-[5px] mb-[50px] border-[#008000]' />
+                </div> {/*Card8 ends here */} 
+    
+            </div> {/*Cards 7 and 8 Ends Here */}
 
         </div> {/*SetOfQuestions Ends Here */}
 
-        <div className='PostTheQn'>
-            <div>
-                <p className='text-[29px]  lg:text-[45px]   lg:text-start font-[700] font-inter  text-[#008000] mb-[10px] lg:mb-0'>Can't Find What You Are Looking For?</p>
+        {/*User Input Section */}
+        <div className='PostTheQn mt-[100px] border-solid border-[1px] border-[#b3b5bc] shadow-md shadow-[#4e4e50] rounded-[20px] '>
+            <div className='flex justify-center'>
+                <p className='text-[29px] lg:text-[45px] pt-[20px]  lg:text-start font-[700] font-inter   text-[#008000] mb-[10px] lg:mb-[30px]'>Can't Find What You Are Looking For?</p>
             </div>
-
-            <div>
-               <textarea placeholder="Post Your Question" className="h-[220px] w-[80vw] lg:h-[220px] lg:w-[474px]  rounded-[8px] border-solid border-[1px] border-[#D5DAE1] focus:outline-none text-[16px] lg:text-[24px] font-inter px-[14px] py-[10px]"  /> 
-            </div>     
-
-            <div className="flex justify-center mx-[2vw] lg:mx-0">
-                <button className="h-[56px] w-[80vw] lg:w-[474px] rounded-[14px] bg-[#008000] border-none font-inter text-[25px]  text-white font-[700] mt-[41px] cursor-pointer active:scale-90 transition-all duration-500 ease-out">Send</button>
+            <div className='mx-auto w-[80%] mt-[20px] mb-[50px] border-solid border-[1px] border-[#b3b5bc] shadow-md rounded-[20px] overflow-clip'>
+                <textarea placeholder="Post Your Question" className="block h-[220px] w-[97.9%] lg:h-[220px] border-none  focus:outline-none text-[16px] lg:text-[24px] font-inter  p-[20px]"  /> 
+                <button className="block h-[56px] w-full mx-auto  rounded-[14px] bg-[#008000] border-none font-inter text-[25px]  text-white font-[700]  cursor-pointer active:scale-90 transition-all duration-500 ease-out">Send</button>
             </div>
-
-
-
-
         </div>  {/*PostTheQn Ends Here */}
 
+        <Link to= "/" className="block mt-[70px] pb-[100px]">
+            <button className="h-[80px] w-[250px] rounded-[16px] bg-[#008000] border-none font-inter text-[28px] text-white font-[700]  cursor-pointer active:scale-90 transition-all duration-500 ease-out">BACK</button>
+        </Link>
 
 
         </div>   {/*FaqDetails End Here */}
