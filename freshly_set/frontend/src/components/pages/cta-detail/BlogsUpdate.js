999ffe5f3b78d9fb48fbaccc3c48d39ea443d004
unknown <allanmathenge2.com>
2024-08-16 13:49:00 +0300

    search

---
+++ freshly_set/frontend/src/components/pages/cta-detail/BlogsUpdate.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import SearchBlogsUpdate from "./SearchBlogsUpdate"
+
+export default function BlogsUpdate() {
+  return (
+    <>
+      <main className='max-w-[80%] my-8 mx-auto'>
+        <SearchBlogsUpdate />
+        <section>
+
+        </section>
+      </main>
+    </>
+  )
+}
