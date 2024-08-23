1da246c630bbfb506a83dc2741164cb33fe2dfd1
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-08-23 21:12:23 +0300

    Fixing the Mobile and Desktop Vertion of WHy Choose freshly section

--- freshly_set/frontend/src/App.js
+++ freshly_set/frontend/src/App.js
@@ -7,7 +7,6 @@ import Home from "./components/pages/homepage/Home"
 import LoginSignUp from "./components/pages/SignUp/LoginSignUp";
 import Blogs from "./components/pages/blogs/Blogs";
 
-import BlogsAllArticles from './components/pages/cta-detail/BlogsAllArticles';
 import Products from './components/pages/farm-produce/Products';
 import FarmingSystemsDetail from './components/pages/farm-produce/FarmingSystemsDetail';
 import GardenSetupsDetail from './components/pages/farm-produce/GardenSetupsDetail';
