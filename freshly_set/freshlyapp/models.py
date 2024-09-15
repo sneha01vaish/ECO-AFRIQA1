8eb36eca9f98f71ce189efe2c11515c391802100
John Mungai <ndungumjohn86@gmail.com>
2024-09-15 16:55:50 +0300

    order

--- freshly_set/freshlyapp/models.py
+++ freshly_set/freshlyapp/models.py
@@ -398,6 +398,19 @@ class IDVerification(models.Model):
         
         return False
     
+from django.db import models
+from django.contrib.auth.models import User
+from .models import Product  # Ensure Product model is already defined
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_qty = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.username}'s Cart"
+
+    
 
 
 # Order model to store user and order details
