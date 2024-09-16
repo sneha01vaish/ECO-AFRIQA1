cb0a35dc649ae5098280cafb39e7ea34da1d5d08
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-09-16 21:11:16 +0300

    Order Progress Page Mobile Version

--- freshly_set/frontend/src/components/pages/OrderManagement/OrderItemCard.js
+++ freshly_set/frontend/src/components/pages/OrderManagement/OrderItemCard.js
@@ -2,43 +2,43 @@ import React from "react";
 
 function OrderItemCard({img, category, name, quantity, status}) {
     return (
-        <div className="OrderItemCard mt-[20px]">
-            <div className="OrderItemWrapper grid grid-cols-2">
+        <div className="OrderItemCard mt-[16px] lg:mt-[20px]">
+            <div className="OrderItemWrapper grid grid-cols-2 gap-[30px]">
                 {/*Item Picture, Category and Name */}
                 <div className="ItemPictureAndName block">
                     <div className="ItemPictureAndNameWrapper flex justify-start">
                         {/*Item Picture Card */}
-                        <div className="ItemPictureCard rounded-[6px] bg-[#00AA5B1A] mr-[20px] ">
-                            <div className="PictureWrapper w-[70px] h-[58px] py-[20px] px-[10px] "> 
+                        <div className="ItemPictureCard rounded-[6px] bg-[#00AA5B1A] mr-[4px] lg:mr-[20px] ">
+                            <div className="PictureWrapper w-[35px] lg:w-[70px] h-[28px] lg:h-[58px] py-[10px] lg:py-[20px] px-[10px] "> 
                                 <img src={img} alt={name} className="w-full h-full" />
                             </div>
                         </div>
                         {/*Item category */}
-                        <div className="ItemCategory my-[45px] mr-[6px] ">
-                            <p className="text-start font-inter font-[700] text-[14px] text-[#008000] my-0 ">{category}</p>
+                        <div className="ItemCategory my-[22px] lg:my-[45px] mr-[2px] lg:mr-[6px] ">
+                            <p className="text-start font-inter font-[700] text-[10px] lg:text-[14px] text-[#008000] my-0 ">{category}</p>
                         </div>
                         {/*Period Seperator */}
-                        <div className="PeriodSeperator my-[45px] mr-[6px]">
-                            <p className="text-start font-inter font-[700] text-[14px] text-[#000000] my-0 "> . </p>
+                        <div className="PeriodSeperator my-[22px] lg:my-[45px] mr-[2px] lg:mr-[6px]">
+                            <p className="text-start font-inter font-[700] text-[10px] lg:text-[14px] text-[#000000] my-0 "> . </p>
                         </div>
                         {/*Item Name */}
-                        <div className="ItemName my-[45px] mr-[6px] ">
-                            <p className="text-start font-inter font-[700] text-[14px] text-[#000000] my-0 ">{name}</p>
+                        <div className="ItemName my-[22px] lg:my-[45px] mr-[2px] lg:mr-[6px] ">
+                            <p className="text-start font-inter font-[700] text-[10px] lg:text-[14px] text-[#000000] my-0 ">{name}</p>
                         </div>
                         
                     </div> {/*Item Picture Wrapper Ends Here */}
                 </div> {/*Item Picture and Name Ends Here */}
 
                 {/*Item Quantity And Order Status */}
-                <div className="ItemQttyAndStatus block my-[45px]">
+                <div className="ItemQttyAndStatus block my-[22px] lg:my-[45px]">
                     <div className="ItemQttyAndStatusWrapper grid grid-cols-2">
                         {/*Items Quantity */}
                         <div className="ItemQtty">
-                            <p className="text-center font-inter font-[700] text-[16px] text-[#008000] my-0">{quantity}</p>
+                            <p className="text-center font-inter font-[700] text-[10px] lg:text-[16px] text-[#008000] my-0">{quantity}</p>
                         </div>
                         {/*Items Order Status*/}
                         <div className="ItemStatus">
-                            <p className="text-center font-inter font-[700] text-[16px] text-[#FF0C1A] my-0"> {status}</p>
+                            <p className="text-center font-inter font-[700] text-[10px] lg:text-[16px] text-[#FF0C1A] my-0"> {status}</p>
                         </div>
                     </div> {/*Items Quantity and Status Wrapper Ends Here */}
                 </div> {/*Items Quantity And Status Ends Here */}
--- freshly_set/frontend/src/components/pages/OrderManagement/OrderProgress.js
+++ freshly_set/frontend/src/components/pages/OrderManagement/OrderProgress.js
@@ -26,14 +26,14 @@ function OrderProgress() {
     }
 
     return (
-        <div className="OrderProgress mb-[200px] mt-[200px]">
+        <div className="OrderProgress mb-[200px] mt-[150px] lg:mt-[200px]">
             {/*Navbar */}
             <div className="The Navbar">
                 <Nav />
             </div>
 
             {/*Back Button */}
-            <div className="mx-[40px] w-fit" >
+            <div className="mx-[12px] lg:mx-[40px] w-fit" >
                 <Link to="/notification" >
                     <div className="ButtonWrapper w-[61px] h-[47px] cursor-pointer">
                         <img src="/static/media/image10.png"  alt="BackButton" className="w-full h-full"/>
@@ -42,79 +42,79 @@ function OrderProgress() {
             </div>
 
             {/*Order Status */}
-            <div className="OrderStatusBanner bg-gradient-to-r from-[#007000] to-[#000A00] shadow-md shadow-[#00000040] mx-[14px] mt-[20px] pb-[50px] lg:pb-[20px] rounded-[29px]">
-                <div className="OrderStatusBannerWrapper px-[30px] py-[30px]">
+            <div className="OrderStatusBanner bg-gradient-to-r from-[#007000] to-[#000A00] shadow-md shadow-[#00000040] mx-[4px] lg:mx-[14px] mt-[20px] lg:pb-[20px] rounded-[15px] lg:rounded-[29px]">
+                <div className="OrderStatusBannerWrapper px-[10px] lg:px-[30px] py-[30px]">
                     
                     {/*Progress Title */}
                     <div className="TitleWrapper block">
-                        <h3 className="text-start font-[700] font-inter text-[10px] lg:text-[35px] text-[#FFFFFF] my-0">Your Order Progress</h3>
+                        <h3 className="text-start font-[700] font-inter text-[20px] lg:text-[35px] text-[#FFFFFF] my-0">Your Order Progress</h3>
                     </div>
                         
                     {/*ProgressBars */}
                     <div className="ProgressBars block mt-[16px] lg:mt-[28px]">
-                        <div className="ProgressBarsWrapper flex justify-between">
+                        <div className="ProgressBarsWrapper grid grid-cols-4 lg:flex lg:justify-between">
                             {/*OrderConfirmation */}
                             <div className="block">
-                                <div className="OrderConfirmation bg-[#D9D9D9] h-[9px] lg:h-[10px] w-[50px] lg:w-[247px] ">
+                                <div className="OrderConfirmation bg-[#D9D9D9] h-[9px] lg:h-[10px] w-[60px] lg:w-[247px] ">
                                     <div className={`Progress h-full ${ProcessingColor(OrderConfirmation)}`} style={{ width: `${OrderConfirmation}%` }}></div>                                  {/*Dynamic Progress bar length and color*/}
                                 </div>
                                 <div className="OrderTag">
-                                    <p className="text-start text-[20px] font-inter font-[700] bg-gradient-to-b bg-clip-text text-transparent from-[#ffffff] to-[#999999]">Order Confirmation</p>
+                                    <p className="text-start text-[9px] lg:text-[20px] font-inter font-[700] bg-gradient-to-b bg-clip-text text-transparent from-[#ffffff] to-[#999999]">Order Confirmation</p>
                                 </div>
                             </div>
 
                             {/*OrderPayment */}
                             <div className="block">
-                                <div className="OrderPayment bg-[#D9D9D9] h-[9px] lg:h-[10px] w-[50px] lg:w-[247px] ">
+                                <div className="OrderPayment bg-[#D9D9D9] h-[9px] lg:h-[10px] w-[60px] lg:w-[247px] ">
                                     <div className={`Progress h-full ${ProcessingColor(OrderPayment)}`} style={{ width: `${OrderPayment}%` }}></div>                                  {/*Dynamic Progress bar length and color*/}
                                 </div>
                                 <div className="OrderTag">
-                                    <p className="text-start text-[20px] font-inter font-[700] bg-gradient-to-b bg-clip-text text-transparent from-[#ffffff] to-[#999999]">Payment</p>
+                                    <p className="text-start text-[9px] lg:text-[20px] font-[700] font-inter bg-gradient-to-b bg-clip-text text-transparent from-[#ffffff] to-[#999999]">Payment</p>
                                 </div>
                             </div>
 
                             {/*OrderProcessing */}
                             <div className="block">
-                                <div className="OrderProcessing bg-[#D9D9D9] h-[9px] lg:h-[10px] w-[50px] lg:w-[247px] ">
+                                <div className="OrderProcessing bg-[#D9D9D9] h-[9px] lg:h-[10px] w-[60px] lg:w-[247px] ">
                                     <div className={`Progress h-full ${ProcessingColor(OrderProcessing)}`} style={{ width: `${OrderProcessing}%` }}></div>                                  {/*Dynamic Progress bar length and color*/}
                                 </div>
                                 <div className="OrderTag">
-                                    <p className="text-start text-[20px] font-inter font-[700] bg-gradient-to-b bg-clip-text text-transparent from-[#ffffff] to-[#999999]">Processing</p>
+                                    <p className="text-start text-[9px] lg:text-[20px] font-[700] font-inter bg-gradient-to-b bg-clip-text text-transparent from-[#ffffff] to-[#999999]">Processing</p>
                                 </div>
                             </div>
                             
                             {/*OrderDelivery */}
                                 <div className="block">
-                                <div className="OrderDelivery bg-[#D9D9D9] h-[9px] lg:h-[10px] w-[50px] lg:w-[247px] ">
+                                <div className="OrderDelivery bg-[#D9D9D9] h-[9px] lg:h-[10px] w-[60px] lg:w-[247px] ">
                                     <div className={`Progress h-full ${ProcessingColor(OrderDelivery)}`} style={{ width: `${OrderDelivery}%` }}></div>                                  {/*Dynamic Progress bar length and color*/}
                                 </div>
                                 <div className="OrderTag">
-                                    <p className="text-start text-[20px] font-inter font-[700] bg-gradient-to-b bg-clip-text text-transparent from-[#ffffff] to-[#999999]">Delivery</p>
+                                    <p className="text-start text-[9px] lg:text-[20px] font-inter font-[700] bg-gradient-to-b bg-clip-text text-transparent from-[#ffffff] to-[#999999]">Delivery</p>
                                 </div>
                             </div>
                         </div> {/*Progress Bars Wrapper Ends Here */}
                     </div> {/*Progress Bars Ends Here */}
 
-                    {/*Order Delivery DAte And Time */}
-                    <div className="DeliveryDateAndTime mt-[40px] py-[16px] px-[14px] bg-[#FFFFFF] rounded-[16px] w-fit border-solid border-[3px] border-[#008000] ">
-                        <p className="text-start font-[700] font-inter text-[10px] lg:text-[20px] text-[#000000] my-0"> Estimated Delivery Date And Time: {Date}, {Time} </p>
-                    </div>
-                   
-                   
+                    {/*Order Delivery Date And Time */}
+                    <div className="DeliveryDateAndTime mt-[40px] py-[4px] lg:py-[16px] px-[6px] lg:px-[14px] bg-[#FFFFFF] rounded-[6px] lg:rounded-[16px] lg:w-fit border-solid border-[1px] lg:border-[3px] border-[#008000] ">
+                        <p className="text-start font-[700] font-inter text-[9px] lg:text-[20px] text-[#000000] my-0"> Estimated Delivery Date And Time: {Date}, {Time} </p>
+                    </div>                  
                 </div> {/*OrderSatusBanner Wrapper ends Here */}
             </div> {/*Orders Status Banner Ends Here */}
 
             {/*Order Items progress Cards */}
             <div className="OrderProgressWrapper mt-[30px] mx-[12px] lg:mx-[200px] px-[14px] lg:px-[28px] py-[10px] lg:py-[24px] rounded-[8px] lg:rounded-[18px] border-solid border-[0.6px] border-[#A6B1AD] shadow-md shadow-[#00000040]">
+                
                 {/*Tittle of Order Progress */}
-                <div className="OrderProgressTitle">
-                    <p className="text-start font-inter font-[700] text-[28px] text-[#008000] my-0 ">Your Items</p>
+                <div className="OrderProgressTitle mt-[10px]">
+                    <p className="text-start font-inter font-[700] text-[15px] lg:text-[28px] text-[#008000] my-0 ">Your Items</p>
                 </div>
+                
                 {/*Titles of Sections */}
-                <div className="OrderItemWrapper grid grid-cols-2 mt-[20px]">
+                <div className="OrderItemWrapper grid grid-cols-2 gap-[30px] mt-[20px]">
                 {/*Item Picture, Category and Name */}
                 <div className="ItemPictureAndName block">
-                    <p className="text-start font-inter font-[700] text-[16px] text-[#00000] my-0 ">Item</p>
+                    <p className="text-start font-inter font-[700] text-[12px] lg:text-[16px] text-[#00000] my-0 ">Item</p>
                 </div> {/*Item Picture and Name Ends Here */}
 
                 {/*Item Quantity And Order Status */}
@@ -122,11 +122,11 @@ function OrderProgress() {
                     <div className="ItemQttyAndStatusWrapper grid grid-cols-2">
                         {/*Items Quantity */}
                         <div className="ItemQtty">
-                            <p className="text-center font-inter font-[700] text-[16px] text-[#000000] my-0">Quantity</p>
+                            <p className="text-center font-inter font-[700] text-[12px] lg:text-[16px] text-[#000000] my-0">Quantity</p>
                         </div>
                         {/*Items Order Status*/}
                         <div className="ItemStatus">
-                            <p className="text-center font-inter font-[700] text-[16px] text-[#000000] my-0"> Status</p>
+                            <p className="text-center font-inter font-[700] text-[12px] lg:text-[16px] text-[#000000] my-0"> Status</p>
                         </div>
                     </div> {/*Items Quantity and Status Wrapper Ends Here */}
                 </div> {/*Items Quantity And Status Ends Here */}
