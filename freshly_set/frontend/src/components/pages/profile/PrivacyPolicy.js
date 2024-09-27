952fe3940cec2e197c8de9c88115824422b75496
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-09-27 09:36:07 +0300

    The Starter Components for Side navbar

---
+++ freshly_set/frontend/src/components/pages/profile/PrivacyPolicy.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PrivacyPolicy() {
+    return (
+        <div className="PrivacyPolicy">
+            Privacy Policy
+        </div>
+    )
+}
+
+export default PrivacyPolicy
---
+++ freshly_set/frontend/src/components/pages/profile/TermsConditions.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TermsConditions() {
+    return (
+        <div className="TermsConditions">
+            Terms and Conditions
+        </div>
+    )
+}
+
+export default TermsConditions
