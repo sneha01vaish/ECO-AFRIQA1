8df569c8cca8c68833484626db9817a5236a8543
ALLAN MATHENGE <allanmathenge2@gmail.com>
2024-09-27 10:20:45 +0300

    Transporters page

--- freshly_set/frontend/src/components/pages/profile/Transporters.js
+++ freshly_set/frontend/src/components/pages/profile/Transporters.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { IoStarSharp } from "react-icons/io5";
+import { FaEdit } from "react-icons/fa";
 
 function Transporters() {
   return (
-    <div>Transporters</div>
+    <section className="font-inter">
+      <div>
+
+        <h2 className="flex gap-5 font-bold m-0">
+          <span className="capitalize text-[#008000]">
+            Hello jane, welcome to your dashboard
+          </span>
+          <Link
+            href="/delivery"
+            className="text-[#00000080]/50 text-[15px] hover:underline"
+          >
+            View and manage your deliveries and earnings
+          </Link>
+        </h2>
+      </div>
+
+      <div className="flex w-[88%] justify-between">
+        <p className="flex flex-col">
+          <span className="text-start text-red-600 font-bold">5.5 Ratings</span>
+          <span className="flex gap-[1.5px] text-green-600">
+            <IoStarSharp />
+            <IoStarSharp />
+            <IoStarSharp />
+            <IoStarSharp />
+            <IoStarSharp />
+          </span>
+        </p>
+        <p className="font-bold text-green-600">200 Deliveries</p>
+        <p className="font-bold text-green-600">Lorry Automobile</p>
+        <p className="text-3xl"><FaEdit /></p>
+      </div>
+    </section>
   )
 }
 
